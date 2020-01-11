ba8f2dc6cb5771fdf1f3c0e122788ccccb07ef59
Marko Shiva Pavlovic <marko.shiva.pavlovic@gmail.com>
2020-01-11 16:06:22 +0100

    added back header as it was removed from the git

---
+++ Razlomak.h
@@ -0,0 +1,55 @@
+// vim: set undofile:
+// Program: razlomak
+
+#ifndef RAZLOMAK_H
+#define RAZLOMAK_H
+
+#include <iostream>
+
+class Razlomak {
+public:
+    Razlomak(int brojilac = 0, unsigned int imenilac = 1);
+
+    int brojilac() const;
+    void postaviBrojilac(int brojilac);
+
+    unsigned int imenilac() const;
+    void postaviImenilac(unsigned int imenilac);
+
+    Razlomak operator + (const Razlomak & other) const;
+
+    // razlomak++ operator
+    Razlomak operator ++ (int);
+
+    // ++razlomak operator
+    Razlomak & operator ++ ();
+
+    // Unirani operator -
+    Razlomak operator - () const;
+
+    // Binarni operator -
+    Razlomak operator - (const Razlomak & other) const;
+
+    // Operator konverzije u double
+    operator double () const;
+
+    // Operatori jednakosti i nejednakosti
+    bool operator == (const Razlomak & other) const;
+    bool operator != (const Razlomak & other) const;
+
+    // Za vezbu: implementirati mnozenje, deljenje razlomaka
+    // operatore poredjenja - <, >, <=, >=,
+    // mnozenje i deljenje skalarom...
+
+private:
+    int m_brojilac;
+    unsigned int m_imenilac;
+
+    friend std::ostream & operator << (std::ostream & out, const Razlomak & value);
+    friend std::istream & operator >> (std::istream & in, Razlomak & value);
+};
+
+std::ostream & operator << (std::ostream & out, const Razlomak & value);
+std::istream & operator >> (std::istream & in, Razlomak & value);
+
+#endif // RAZLOMAK_H
